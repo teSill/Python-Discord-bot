51da7b90e87e1188f5393feabfb5917db44e0ab5
Teemu Sillanpää <teemu@unicornpirates.studio>
2020-04-06 10:36:31 +0300

    on_command_error now sends a message to the chat

--- discord_bot.py
+++ discord_bot.py
@@ -6,31 +6,30 @@ from discord.ext import commands
 TOKEN = os.environ.get('DISCORD_TOKEN')
 custom_commands = ["find", "commands", "popular", "findactor", "findactress", "findmovie"]
 
-client = commands.Bot(command_prefix='!temflix ')
+bot = commands.Bot(command_prefix='!temflix ')
 
 
-@client.event
+@bot.event
 async def on_ready():
     print("Temflix has come online!")
 
 
-@client.command(pass_context=True)
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(error)
+
+
+@bot.command(pass_context=True)
 async def load(ctx, extension):
-    client.load_extension(f"cogs.{extension}")
+    bot.load_extension(f"cogs.{extension}")
 
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def unload(ctx, extension):
-    client.unload_extension(f"cogs.{extension}")
-
-for filename in glob.iglob('./cogs/**', recursive=True):
-    if filename.endswith(".py"):
-        file_name = os.path.basename(filename[:-3])
-        parent_folder_name = PurePath(filename).parent.name
-        client.load_extension(f"cogs.{parent_folder_name}.{file_name}")
+    bot.unload_extension(f"cogs.{extension}")
 
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def on_message(self, message):
     if message.author == self.user:
         return
@@ -38,4 +37,11 @@ async def on_message(self, message):
     message.content = message.content.lower()
 
 
-client.run(TOKEN)
+# Load cogs
+for filename in glob.iglob('./cogs/**', recursive=True):
+    if filename.endswith(".py"):
+        file_name = os.path.basename(filename[:-3])
+        parent_folder_name = PurePath(filename).parent.name
+        bot.load_extension(f"cogs.{parent_folder_name}.{file_name}")
+
+bot.run(TOKEN)
